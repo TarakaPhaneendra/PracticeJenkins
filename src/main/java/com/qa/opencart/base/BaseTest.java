fe88b4f5dda0e33cc71b0a012c92c35ab2dbd129
TarakaPhaneendra <1234taraka@gmail.com>
2025-04-19 12:01:14 +0530

    seventh

--- src/main/java/com/qa/opencart/base/BaseTest.java
+++ src/main/java/com/qa/opencart/base/BaseTest.java
@@ -3,8 +3,9 @@ package com.qa.opencart.base;
 import java.util.Properties;
 
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import com.microsoft.playwright.Page;
 import com.qa.opencart.factory.PlaywrightFactory;
@@ -20,16 +21,21 @@ protected Properties prop;
 protected HomePage homePage;
 protected LoginPage loginPage;
 
+@Parameters({"browser"})
 @BeforeTest
-public void setup() {
+public void setup(@Optional("chromium") String browserName) {
 	pf = new PlaywrightFactory();
 	prop = pf.init_prop();
+	if(browserName != null) {
+		prop.setProperty("browser", browserName);
+		
+	}
 	page = pf.initBrowser(prop);
 	homePage = new HomePage(page);
 }
 
 @AfterTest
 public void tearDown() {
-	//page.context().browser().close();
+	page.context().browser().close();
 }
 }
